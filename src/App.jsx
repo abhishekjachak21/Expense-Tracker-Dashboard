d54215d3da3ea84f154fc1aaed817dd4b5d624bc
Abhishek Jachak <abhishekjachak22@gmail.com>
2026-09-13 17:25:21 +0530

    CSS updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import heroImg from './assets/hero.png'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
+import StatCard from './components/StatCard'
+
 import './App.css'
 
 function App() {
@@ -9,6 +11,13 @@ function App() {
     <div>
       <h1>Expense Tracker</h1>
       <p>Manage your income and expenses</p>
+
+            <div className="stats-container">
+        <StatCard title="Balance" value="₹42,500" />
+        <StatCard title="Income" value="₹60,000" />
+        <StatCard title="Expenses" value="₹17,500" />
+      </div>
+
     </div>
   )
 }
