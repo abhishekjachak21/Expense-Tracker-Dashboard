34b166b1fb79c8cb88882183c19286de3805cf9c
Abhishek Jachak <112698837+abhishekjachak21@users.noreply.github.com>
2026-09-21 19:56:16 +0530

    Add backend layer: TransactionNotFoundException.java

---
+++ backend/src/main/java/com/expensetracker/transaction/TransactionNotFoundException.java
@@ -0,0 +1,8 @@
+package com.expensetracker.transaction;
+
+public class TransactionNotFoundException extends RuntimeException {
+
+    public TransactionNotFoundException(Long id) {
+        super("Transaction not found: " + id);
+    }
+}
